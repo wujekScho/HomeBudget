feb862196d8c55f3f6b520cb27c9dd46caca7bcd
wujekScho <schodziak@gmail.com>
2019-09-29 19:28:13 +0200

    Expense category - created dto validation constraints.

--- src/main/java/pl/piotrschodzinski/homebudget/dto/ExpenseCategoryDto.java
+++ src/main/java/pl/piotrschodzinski/homebudget/dto/ExpenseCategoryDto.java
@@ -5,14 +5,19 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ExpenseCategoryDto {
+    @PositiveOrZero
     Long id;
+    @NotBlank
     String name;
     List<ExpenseDto> expenses;
+    @PositiveOrZero
     Long userId;
 }
